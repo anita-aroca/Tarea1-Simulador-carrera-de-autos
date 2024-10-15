7b63c49da446a1c5719a6be0bf53814c6884a3c7
Javier González <javier.gonzalez2101@alumnos.ubiobio.cl>
2024-10-14 21:16:51 -0300

    MOD main.cpp

--- tarea1/main.cpp
+++ tarea1/main.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
+#include <cstdlib>
+#include <unistd.h>
+#include <thread>
+#include <mutex>
+#include <chrono>
+#include <vector>
+
+using namespace std;
+using namespace chrono;
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    cout << "Hello, World!" << endl;
     return 0;
 }
