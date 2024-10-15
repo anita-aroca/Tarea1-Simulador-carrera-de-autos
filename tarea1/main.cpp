58178638fd207df8588db33bc89d1f73ce98545c
Javier González <javier.gonzalez2101@alumnos.ubiobio.cl>
2024-10-15 01:11:41 -0300

    MOD main.cpp

--- tarea1/main.cpp
+++ tarea1/main.cpp
@@ -14,7 +14,8 @@
 #include <random>
 
 using namespace std; // cout endl
-using namespace chrono;
+using namespace chrono; //tiempo, unidades de medida
+using namespace this_thread;
 std::mutex mtx;
 //#define verde "\033[32m" :v
 #define morao "\033[35m"
@@ -36,13 +37,34 @@ int funcRandom(int a, int b) {
 }
 
 // Función para avanzar un auto
+void avanzarAuto(Auto& corredor, int M) {
+    // Mientras el auto no haya alcanzado la meta (M metros)
+    while (corredor.distRec < M) {
+        // Generar avance aleatorio entre 1 y 10 metros
+        int avanza = funcRandom(1,  10);
+        // Generar tiempo aleatorio entre 100 y 500 milisegundos
+        int tiempo = funcRandom(100, 500);
+        // Protege/Bloquea la impresión con un mutex para evitar colisiones en la salida
+        mtx.lock();
+        corredor.distRec += avanza;
+        cout << morao << "Auto" << corredor.id << " avanza " << avanza << " metros (total: " << corredor.distRec << " metros)" << reset << endl;
+        //Libera mutex después de imprimir
+        mtx.unlock();
 
+        // Si alcanza o supera la meta, termina la carrera.
+        if (corredor.distRec >= M) {
+            cout << morao <<"¡Auto" << corredor.id << " ha terminado la carrera!" << endl;
+            break;
+        }
 
+        // Dormir el tiempo aleatorio generado
+        sleep_for(milliseconds(tiempo));
+    }
+}
 
-
-int main(int argc, char argv)
-{
-    cout << "Hello, World!" << endl;
+// Función principal
+int main(int argc, char* argv[]) {
+    cout << morao << "Hello, World!" << reset << endl;
     return 0;
 }
 
